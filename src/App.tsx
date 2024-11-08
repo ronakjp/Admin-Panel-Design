0653888c472167771cd7064cd255612e48c24fd2
ronakjp <ronak.p@thinkwik.com>
2024-11-08 13:20:18 +0530

    [fix] added the basic table

--- src/App.tsx
+++ src/App.tsx
@@ -23,18 +23,18 @@ function App() {
 
   return (
     <>
-      <div className="h-svh max-h-svh w-full bg-background">
+      <div className="min-h-screen w-full bg-background">
         <Header
           handleSideMenuToggle={handleToggle}
           isSidebarOpen={isSidebarOpen}
         />
         <div className="flex flex-row">
           <div
-            className={`inline-block h-screen rounded-e-lg  bg-white shadow-gray-800`}
+            className={`inline-block rounded-e-lg  bg-white shadow-gray-800`}
           >
             <Sidebar isSidebarOpen={isSidebarOpen} />
           </div>
-          <div className="flex-[1] w-full  h-screen  bg-background ">
+          <div className="flex-[1] w-full h-full bg-background ">
             <Container />
           </div>
         </div>
